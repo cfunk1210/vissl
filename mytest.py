0eb548630a80263ffbfd25359b079c03f90fed16
joncrall <jon.crall@kitware.com>
2021-06-11 23:16:02 -0400

    wip

--- mytest.py
+++ mytest.py
@@ -1,5 +1,9 @@
 """
-My testing commands
+
+
+
+INSTALL DEPS
+============
 
 pip install git+https://github.com/facebookresearch/ClassyVision.git
 pip install apex
@@ -14,19 +18,18 @@ pip install -v --disable-pip-version-check --no-cache-dir --global-option="--cpp
 
 
 
-Relevant files:
-    ~/code/vissl/vissl/config/defaults.yaml
-    ~/code/vissl/configs/config/test/integration_test/quick_simclr.yaml
-    ~/code/vissl/configs/config/dataset_catalog.json
-    ~/code/vissl/vissl/data/kwcoco_dataset.py
+Relevant files
+==============
+~/code/vissl/vissl/config/defaults.yaml
+~/code/vissl/configs/config/test/integration_test/quick_simclr.yaml
+~/code/vissl/configs/config/dataset_catalog.json
+~/code/vissl/vissl/data/kwcoco_dataset.py
 
-    ~/code/kwcoco/kwcoco/data/grab_cifar.py
+~/code/kwcoco/kwcoco/data/grab_cifar.py
 
 
-cd $HOME/code/vissl
-
-python tools/run_distributed_engines.py --help
-
+Demo CIFAR
+==========
 
 python -m kwcoco.data.grab_voc
 python -m kwcoco.data.grab_cifar
@@ -40,19 +43,30 @@ kwcoco split \
     --rng 11719619003825326796890461364078 \
     --factor 3
 
-$HOME/.cache/kwcoco/data/cifar10/cifar10-split1.kwcoco.json
-$HOME/.cache/kwcoco/data/cifar10/cifar10-split2.kwcoco.json
+kwcoco stats $HOME/.cache/kwcoco/data/cifar10/cifar10-split1.kwcoco.json
+kwcoco stats $HOME/.cache/kwcoco/data/cifar10/cifar10-split2.kwcoco.json
 
+
+# TODO: this should be available via python -m vissl.__main__
 python \
-    tools/run_distributed_engines.py \
+    $HOME/code/vissl/tools/run_distributed_engines.py \
     config=test/integration_test/quick_simclr.yaml \
     config.CHECKPOINT.DIR="./my_toy_training_v2" \
     config.DATA.TRAIN.DATA_SOURCES=[kwcoco] \
-    config.DATA.TRAIN.DATA_PATHS=[$COCO_FPATH] \
     config.DATA.TRAIN.DATASET_NAMES=[kwcoco] \
     config.DATA.TEST.DATA_SOURCES=[kwcoco] \
-    config.DATA.TEST.DATA_PATHS=[$COCO_FPATH] \
     config.DATA.TEST.DATASET_NAMES=[kwcoco] \
+    config.DATA.TRAIN.DATA_PATHS=[$HOME/.cache/kwcoco/data/cifar10/cifar10-split1.kwcoco.json] \
+    config.DATA.TEST.DATA_PATHS=[$HOME/.cache/kwcoco/data/cifar10/cifar10-split1.kwcoco.json] \
+
+
+
+Debug Stuff
+===========
+
+cd $HOME/code/vissl
+
+python tools/run_distributed_engines.py --help
 
 
 
@@ -77,19 +91,18 @@ python \
     $HOME/code/vissl/tools/run_distributed_engines.py \
     config=benchmark/low_shot_transfer/cifar100/eval_resnet_8gpu_transfer_cifar100_low_tune.yaml \
     config.CHECKPOINT.DIR="$HOME/work/vissl/cifar10_train" \
-    config.NUM_DATALOADER_WORKERS = 0 \
-    config.DATA.TRAIN.BATCHSIZE_PER_REPLICA = 3 \
-    config.DATA.TEST.BATCHSIZE_PER_REPLICA = 3 \
+    config.DISTRIBUTED.NUM_NODES=1 \
+    config.DISTRIBUTED.NUM_PROC_PER_NODE=1 \
+    config.MACHINE.DEVICE=gpu \
+    confie.DATA.NUM_DATALOADER_WORKERS=0 \
+    config.DATA.TRAIN.BATCHSIZE_PER_REPLICA=3 \
+    config.DATA.TEST.BATCHSIZE_PER_REPLICA=3 \
     config.DATA.TRAIN.DATA_PATHS=[$HOME/.cache/kwcoco/data/cifar10/cifar10-split1.kwcoco.json] \
     config.DATA.TEST.DATA_PATHS=[$HOME/.cache/kwcoco/data/cifar10/cifar10-split1.kwcoco.json] \
     config.DATA.TRAIN.DATA_SOURCES=[kwcoco] \
     config.DATA.TRAIN.DATASET_NAMES=[kwcoco] \
     config.DATA.TEST.DATA_SOURCES=[kwcoco] \
     config.DATA.TEST.DATASET_NAMES=[kwcoco]
-
-
-    config.DATA.TRAIN.DATA_SOURCES=[synthetic] \
-    config.CHECKPOINT.DIR="./checkpoints"
 
 
 
